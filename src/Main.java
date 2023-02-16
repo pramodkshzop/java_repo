7e6d26a0b968a72cb759407d0e616c2f183b47f4
pramod <pramod.kumar@zopsmart.com>
2023-02-16 17:24:27 +0530

    added another text

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("this is new text in this file!");
+        System.out.println("this is another text in this file!");
         newClass n = new newClass();
         n.printSomething();
     }
