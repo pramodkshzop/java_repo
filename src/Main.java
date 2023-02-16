e3b3ae18c02ffecfdcf2c4f70c421dc3208c9c81
pramod <pramod.kumar@zopsmart.com>
2023-02-16 15:25:56 +0530

    changed text

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("This is new branch!");
+        System.out.println("This is new branch 2nd commit!");
     }
 }
